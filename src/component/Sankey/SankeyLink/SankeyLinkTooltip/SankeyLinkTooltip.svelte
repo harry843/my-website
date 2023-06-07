1139122c449deb96cbaf608b5e0c12c4e3b2a064
Harry Kelleher <HAKE2@hscic.gov.uk>
2023-06-07 23:04:54 +0100

    push link tooltip  functions to utils

--- src/component/Sankey/SankeyLink/SankeyLinkTooltip/SankeyLinkTooltip.svelte
+++ src/component/Sankey/SankeyLink/SankeyLinkTooltip/SankeyLinkTooltip.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-	import { formatTooltipValue } from '../../../../routes/portfolio/sankey/utils';
+	import {
+		formatTooltipValue,
+		getLinkTooltipText
+	} from '../../../../routes/portfolio/sankey/utils';
 	import { hoverStore, linkTooltipData } from '../../../../stores';
 	import nodevalues from '../../../../routes/portfolio/sankey/nodevalues.json';
 	import classNames from 'classnames';
@@ -10,51 +13,6 @@
 	const nodes = nodevalues.data;
 
 	$: linkTooltipPadding = paddingY > 10 ? 30 : -20;
-
-	function edAbbreviation(object: any) {
-		const regex = /Emergency Department/;
-		const replacement = 'ED';
-
-		if (regex.test(object)) {
-			return object.replace(regex, replacement);
-		}
-
-		return object;
-	}
-
-	const getLinkTooltipText = ($linkTooltipData: Partial<TransformedLink>) => {
-		const { target, source } = $linkTooltipData;
-
-		if (target === 'No attendance') {
-			return `callers did not attend an Emergency Department after receiving a ${edAbbreviation(
-				source
-			)} disposition from NHS111`;
-		}
-
-		if (target === 'Type 1 Emergency Department' || target === 'Types 2-4 Emergency Department') {
-			return `people attended a ${target} who received a ${source} disposition from NHS111`;
-		}
-
-		if ((target === 'Necessary' || target === 'Low acuity') && source) {
-			return `${source} attendances were classified as ${target}`;
-		}
-
-		if (target === 'Admission' && source === 'Necessary') {
-			return `${source.toLowerCase()} attendances at Type 1 Emergency Department resulted in hospital admission`;
-		}
-
-		if (target === 'Admission' && source === 'No attendance') {
-			return `callers did not attend an Emergency Department, but were subsequently admitted to hospital`;
-		}
-
-		if (target === 'Admission' && source === 'Types 2-4 Emergency Department') {
-			return `attendances at ${source} resulted in hospital admission`;
-		}
-
-		if (target === 'No admission') {
-			return `callers had a necessary Type 1 Emergency Department attendance, but were not admitted to hospital`;
-		}
-	};
 </script>
 
 <div
--- src/routes/portfolio/sankey/utils.ts
+++ src/routes/portfolio/sankey/utils.ts
@@ -1,3 +1,5 @@
+import type { TransformedLink } from "../../../types/portfolio";
+
 export const numberWithCommas = (x: number) => {
     return x
         .toFixed(2)
@@ -7,4 +9,49 @@ export const numberWithCommas = (x: number) => {
 
 export const formatTooltipValue = (value: number) => {
     return numberWithCommas(Math.floor(value)).replace('.00', '');
+};
+
+function edAbbreviation(object: any) {
+    const regex = /Emergency Department/;
+    const replacement = 'ED';
+
+    if (regex.test(object)) {
+        return object.replace(regex, replacement);
+    }
+
+    return object;
+}
+
+export const getLinkTooltipText = ($linkTooltipData: Partial<TransformedLink>) => {
+    const { target, source } = $linkTooltipData;
+
+    if (target === 'No attendance') {
+        return `callers did not attend an Emergency Department after receiving a ${edAbbreviation(
+            source
+        )} disposition from NHS111`;
+    }
+
+    if (target === 'Type 1 Emergency Department' || target === 'Types 2-4 Emergency Department') {
+        return `people attended a ${target} who received a ${source} disposition from NHS111`;
+    }
+
+    if ((target === 'Necessary' || target === 'Low acuity') && source) {
+        return `${source} attendances were classified as ${target}`;
+    }
+
+    if (target === 'Admission' && source === 'Necessary') {
+        return `${source.toLowerCase()} attendances at Type 1 Emergency Department resulted in hospital admission`;
+    }
+
+    if (target === 'Admission' && source === 'No attendance') {
+        return `callers did not attend an Emergency Department, but were subsequently admitted to hospital`;
+    }
+
+    if (target === 'Admission' && source === 'Types 2-4 Emergency Department') {
+        return `attendances at ${source} resulted in hospital admission`;
+    }
+
+    if (target === 'No admission') {
+        return `callers had a necessary Type 1 Emergency Department attendance, but were not admitted to hospital`;
+    }
 };
