ced6860de2943f5fa515f1554abae432d724321f
harry843 <kelleherharry@gmail.com>
2023-10-11 19:13:49 +0100

    switch contact for blog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,14 @@
 	import '../app.css';
 	import Sidebar from '../component/NavBar/Footer/Sidebar.svelte';
 	import NavBar from '../component/NavBar/NavBar.svelte';
+	import { page } from '$app/stores';
 </script>
 
 <NavBar />
 <section class="px-2 xs:px-4 md:px-16 lg:px-32 xl:px-44 2xl:px-56 mt-20 py-4">
 	<slot />
 </section>
+{#if $page.url.pathname.slice(1) != 'cv'}
 <Sidebar />
+{/if}
+
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>Blog | Harry Kelleher</title>
+</svelte:head>
+
+
+<script>
+	let width;
+</script>
+
+<section bind:clientWidth={width}
+	class="flex flex-col items-center font-normal px-4 text-sm w-full "
+>
+	<p class="p-2">
+		Hi, I'm Harry. You can contact me on the platforms listed below.
+	</p>
+</section>
