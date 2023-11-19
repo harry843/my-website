249ec8b8fc31fe9d8a199b1cd2c7492edfd681af
harry843 <kelleherharry@gmail.com>
2023-11-19 20:12:36 +0000

    remove autotimer image carousel from home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,59 +5,59 @@
 
 	let width: number = 0;
 
-	const text = [
-		{ name: 'BI developer.', image: 'HK_thumbnail.png', alt: "Harry's profile picture" },
-		{ name: 'nerd?', image: 'HK_Naruto.png', alt: 'Harry as a cartoon' },
-		{ name: 'musician?', image: 'HK_Music.jpg', alt: 'Harry creating music' },
-		{ name: 'enough?', image: 'HK_Ken.png', alt: 'Am I Kenough?' }
-	];
-	let counter: number = 0;
-	let cycles: number = 0;
-	const maxCycles: number = 1;
-	let interval: number | undefined = undefined;
+	// const text = [
+	// 	{ name: 'BI developer.', image: 'HK_thumbnail.png', alt: "Harry's profile picture" },
+	// 	{ name: 'nerd?', image: 'HK_Naruto.png', alt: 'Harry as a cartoon' },
+	// 	{ name: 'musician?', image: 'HK_Music.jpg', alt: 'Harry creating music' },
+	// 	{ name: 'enough?', image: 'HK_Ken.png', alt: 'Am I Kenough?' }
+	// ];
+	// let counter: number = 0;
+	// let cycles: number = 0;
+	// const maxCycles: number = 1;
+	// let interval: number | undefined = undefined;
 
-	function changeText() {
-		counter = (counter + 1) % text.length;
-		if (counter === 0) {
-			cycles++;
-			if (cycles === maxCycles) {
-				firstVisit.set('false');
-				clearInterval(interval);
-				console.log(firstVisit);
-			}
-		}
-	}
+	// function changeText() {
+	// 	counter = (counter + 1) % text.length;
+	// 	if (counter === 0) {
+	// 		cycles++;
+	// 		if (cycles === maxCycles) {
+	// 			firstVisit.set('false');
+	// 			clearInterval(interval);
+	// 			console.log(firstVisit);
+	// 		}
+	// 	}
+	// }
 
-	// Computed property to determine the interval based on conditions
-	$: {
-		clearInterval(interval); // Clear any existing interval
+	// // Computed property to determine the interval based on conditions
+	// $: {
+	// 	clearInterval(interval); // Clear any existing interval
 
-		if (counter === 0 && cycles === 0) {
-			interval = setInterval(changeText, 2150);
-		} else if (counter === text.length - 1 && cycles === 0) {
-			interval = setInterval(changeText, 2500);
-		} else if (counter === text.length - 2 && cycles === 0) {
-			interval = setInterval(changeText, 1900);
-		} else if (counter !== 0 && cycles === 0) {
-			interval = setInterval(changeText, 1450);
-		}
-	}
+	// 	if (counter === 0 && cycles === 0) {
+	// 		interval = setInterval(changeText, 2150);
+	// 	} else if (counter === text.length - 1 && cycles === 0) {
+	// 		interval = setInterval(changeText, 2500);
+	// 	} else if (counter === text.length - 2 && cycles === 0) {
+	// 		interval = setInterval(changeText, 1900);
+	// 	} else if (counter !== 0 && cycles === 0) {
+	// 		interval = setInterval(changeText, 1450);
+	// 	}
+	// }
 
-	$: textStyle =
-		counter === 0 || cycles === maxCycles
-			? 'text-indigo-600 font-medium'
-			: 'text-orange-600 font-medium';
+	// $: textStyle =
+	// 	counter === 0 || cycles === maxCycles
+	// 		? 'text-indigo-600 font-medium'
+	// 		: 'text-orange-600 font-medium';
 
-	$: def = counter === text.length - 1 ? 'I am ' : "Hi, I'm a ";
+	// $: def = counter === text.length - 1 ? 'I am ' : "Hi, I'm a ";
 
-	$: k = counter === text.length - 1 ? '𝓚' : '';
+	// $: k = counter === text.length - 1 ? '𝓚' : '';
 </script>
 
 <svelte:head>
 	<title>Harry Kelleher</title>
 </svelte:head>
 
-{#if $firstVisit === 'true'}
+<!-- {#if $firstVisit === 'true'}
 	<section class="flex flex-col items-center font-normal px-4 py-2 text-lg w-full">
 		<p>
 			{def}<span class="text-3xl text-pink-600 font-medium">{k}</span><span
@@ -77,7 +77,7 @@
 			</div>
 		{/if}
 	</section>
-{:else}
+{:else} -->
 	<section class="flex flex-col-reverse md:flex-row">
 		<div class="w-full xs:w-11/12 xs:translate-x-7 sm:w-3/4 sm:translate-x-20 md:w-1/2 md:translate-x-0" bind:clientWidth={width}>
 			{#if width > 0}
@@ -91,4 +91,4 @@
 			<p>I create data products to help grow your business.</p>
 		</div>
 	</section>
-{/if}
+<!-- {/if} -->
