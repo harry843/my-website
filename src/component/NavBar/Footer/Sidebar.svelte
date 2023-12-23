080a6bb16d9ebdab2a9e9a42d21e437af28a320a
harry843 <kelleherharry@gmail.com>
2023-12-23 12:27:30 +0000

    add footer to all pages except cv

--- src/component/NavBar/Footer/Sidebar.svelte
+++ src/component/NavBar/Footer/Sidebar.svelte
@@ -56,6 +56,18 @@
 		{/each}
 	  	</div>
 	</div>
+	<footer>
+		<div class="px-2 xs:px-4 md:px-16 lg:px-32 xl:px-44 2xl:px-56 xxs:py-8">
+			<div class="flex items-center justify-between px-0 xxs:px-2 md:translate-y-[4.7rem] lg:translate-y-[5.6rem]">
+				<a href="https://harrykelleher.com/" class="flex items-start xs:items-center mb-0 space-x-3 rtl:space-x-reverse grayscale hover:grayscale-0">
+					<img src="/HK_thumbnail.png" class="h-8" alt="Harry Kelleher" />
+					<span class="self-center font-customHeading text-base font-medium whitespace-nowrap">{myName}</span>
+				</a>
+			</div>
+			<hr class="my-6 border-gray-200 mx-auto lg:my-8" />
+			<span class="block text-sm text-gray-500 text-center dark:text-gray-400">© {currentYear} <a href="https://harrykelleher.com/" class="hover:underline">Harry Kelleher</a></span>
+		</div>
+	</footer>
 		{:else}
 		<footer bind:clientWidth={outerWidth}>
 			<div class="w-full max-w-screen-xl mx-auto p-4 xxs:py-8">
@@ -78,7 +90,6 @@
 				<span class="block text-sm text-gray-500 text-center dark:text-gray-400">© {currentYear} <a href="https://harrykelleher.com/" class="hover:underline">Harry Kelleher</a></span>
 			</div>
 		</footer>	
-
 	  {/if}
 
 
