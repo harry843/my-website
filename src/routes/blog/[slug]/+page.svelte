2fd54da12f97cd6f21436b1d87900fb039a7f086
harry843 <kelleherharry@gmail.com>
2023-12-28 15:32:56 +0000

    add metadata to head for SEO & sharing socials

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,6 @@
 <script async defer src="https://cusdis.com/js/cusdis.es.js" lang="ts">
-    import { onMount, createEventDispatcher, afterUpdate, onDestroy } from "svelte";
-	import { browser } from "$app/environment";
+	import { onMount, createEventDispatcher, afterUpdate, onDestroy } from 'svelte';
+	import { browser } from '$app/environment';
 	import CustomHeading from '../../../component/PortableText/CustomHeading.svelte';
 	import CustomParagraph from '../../../component/PortableText/CustomParagraph.svelte';
 	import CustomUrl from '../../../component/PortableText/CustomURL.svelte';
@@ -9,7 +9,6 @@
 	import CustomListItem from '../../../component/PortableText/CustomListItem.svelte';
 	import CustomQuote from '../../../component/PortableText/CustomQuote.svelte';
 	import CustomImage from '../../../component/PortableText/CustomImage.svelte';
-	import Image from '../../../component/Image/Image.svelte';
 	import Tag from '../../../component/Tag/Tag.svelte';
 	import averageReadingTime from '../../../component/Card/BlogPostCard/averageReadingTime';
 	import dateformat from 'dateformat';
@@ -24,45 +23,48 @@
 
 	const dispatch = createEventDispatcher();
 
-const load = () =>{
-	dispatch("load");
-}
+	const load = () => {
+		dispatch('load');
+	};
 
-onDestroy(()=>{
-	if (browser){
-		let url = "https://cusdis-comments-4386.vercel.app/js/cusdis.es.js";
-		let script  = document.querySelector(`script[src="${url}"]`);
-		script.removeEventListener("load", load);
-	}
-})
+	onDestroy(() => {
+		if (browser) {
+			let url = 'https://cusdis-comments-4386.vercel.app/js/cusdis.es.js';
+			let script = document.querySelector(`script[src="${url}"]`);
+			script.removeEventListener('load', load);
+		}
+	});
 
-afterUpdate(async ()=>{
-	load();
-})
+	afterUpdate(async () => {
+		load();
+	});
 
-onMount(async ()=>{
-	let url = "https://cusdis-comments-4386.vercel.app/js/cusdis.es.js";
-        let script  = document.querySelector(`script[src="${url}"]`);
-        if (!script){
-            script = document.createElement("script");
-            script.src = url;
-            script.defer = true;
-            script.async = true;
-            script.addEventListener("load", load);
-            document.querySelector("head").appendChild(script);
-        }
-        window.CUSDIS.initial();
-    })
+	onMount(async () => {
+		let url = 'https://cusdis-comments-4386.vercel.app/js/cusdis.es.js';
+		let script = document.querySelector(`script[src="${url}"]`);
+		if (!script) {
+			script = document.createElement('script');
+			script.src = url;
+			script.defer = true;
+			script.async = true;
+			script.addEventListener('load', load);
+			document.querySelector('head').appendChild(script);
+		}
+		window.CUSDIS.initial();
+	});
 
-	$: console.log(blog?.slug.current);
 </script>
 
 <svelte:head>
 	<title>Blog | {blog?.title}</title>
 	<script async defer src="https://cusdis-comments-4386.vercel.app/js/cusdis.es.js"></script>
+	<meta property="og:title" content={blog?.title} />
+	<meta property="og:image" content={blog?.mainImage?.image?.asset?.url + '?w=1200&h=627'} />
+	<meta property="og:description" content={blog?.feature}/>
+	<meta property="og:url" content={'https://harrykelleher.com/blog/'+blog?.slug.current}/>
 </svelte:head>
 
-<div class="mx-5 md:mx-[15%] lg:mx-[18%] xl:mx-[22%]">
+<section class="mx-2 sm:mx-5 md:mx-[15%] lg:mx-[18%] xl:mx-[22%]">
 	{#if blog?.title}
 		<h1 class="text-3xl font-semibold font-customHeading text-center pb-6">{blog?.title}</h1>
 	{/if}
@@ -131,15 +133,17 @@ onMount(async ()=>{
 		/>
 		<br />
 	{/if}
-</div>
-
-<div
-	id="cusdis_thread"
-	class="scale-100"
-	data-host="https://cusdis-comments-4386.vercel.app"
-	data-app-id="6a1a98be-887b-4480-88ed-0cc2e588698a"
-	data-page-id={blog?.slug.current}
-	data-page-url="https://staging.harrykelleher.com/blog/{blog?.slug.current}"
-	data-page-title={blog?.title}
-	data-theme="light"
-/>
+</section>
+<section class="flex flex-col items-left text-left mx-2 md:mx-[15%] pt-5">
+	<h2 class="py-2 font-customHeading font-medium text-2xl">What's your take?</h2>
+	<p class="pb-4">A penny for your thoughts in the comments below.</p>
+	<div
+		id="cusdis_thread"
+		data-host="https://cusdis-comments-4386.vercel.app"
+		data-app-id="6a1a98be-887b-4480-88ed-0cc2e588698a"
+		data-page-id={blog?.slug.current}
+		data-page-url="https://staging.harrykelleher.com/blog/{blog?.slug.current}"
+		data-page-title={blog?.title}
+		data-theme="light"
+	/>
+</section>
