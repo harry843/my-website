86e18eb475cb0b231b809fe4bff4fa99c63b3f73
harry843 <kelleherharry@gmail.com>
2024-01-01 22:49:45 +0000

    add moon/sun toggle

--- src/component/NavBar/DesktopMenu/DesktopMenu.svelte
+++ src/component/NavBar/DesktopMenu/DesktopMenu.svelte
@@ -1,8 +1,17 @@
 <script>
 	import classNames from 'classnames';
 	import { page } from '$app/stores';
-	import { routes} from '../utils/utils';
+	import { routes } from '../utils/utils';
+	import Sun from '../../Icons/Sun.svelte';
+	import Moon from '../../Icons/Moon.svelte';
 
+	let checked = false;
+
+	function handleCheckboxChange(event) {
+		checked = event.target.checked;
+	}
+
+	$: console.log(checked)
 </script>
 
 <div
@@ -15,15 +24,31 @@
 		{#each routes as route}
 			<li>
 				<a
-					href={'/'+
-					route.slug}
-					class={classNames('block py-2 px-2  text-gray-800 rounded-lg hover:bg-gray-50 hover:rounded-md', {
-						'md:text-indigo-600': route.slug === $page.url.pathname.slice(1)
-					})}
-					aria-current="page">
+					href={'/' + route.slug}
+					class={classNames(
+						'block py-2 px-2  text-gray-800 rounded-lg hover:bg-gray-50 hover:rounded-md',
+						{
+							'md:text-indigo-600': route.slug === $page.url.pathname.slice(1)
+						}
+					)}
+					aria-current="page"
+				>
 					{route.label}</a
 				>
 			</li>
 		{/each}
+		<label class="relative inline-flex items-center justify-center cursor-pointer">
+			<input type="checkbox" bind:checked value="" class="sr-only peer" on:change={handleCheckboxChange} />
+			<div
+				class="w-7 h-[15px] bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[14px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-3 after:h-3 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
+			/>
+			<div>
+				{#if checked}
+					<Moon />
+				{:else}
+				<Sun />
+				{/if}
+			</div>
+		</label>
 	</ul>
 </div>
