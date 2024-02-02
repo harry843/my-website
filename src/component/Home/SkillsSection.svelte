6cdf08263eae45690bb002e9a21a5703134ba6e3
harry843 <kelleherharry@gmail.com>
2024-02-02 08:54:02 +0000

    remove erroneous height on skills section div

--- src/component/Home/SkillsSection.svelte
+++ src/component/Home/SkillsSection.svelte
@@ -73,7 +73,7 @@
 		class="flex flex-col sm:grid sm:grid-rows-[320px_200px] sm:grid-flow-col sm:gap-x-[4rem] md:grid-rows-[320px_180px] md:gap-x-[5rem] lg:gap-x-[10rem]"
 	>
 		{#each [{ name: 'Technical', skill: technical }, { name: 'People', skill: people }, { name: 'Software', skill: software }, { name: 'Languages', skill: languages }] as skills, index}
-			<div class={index % 2 === 0 ? "py-4 h-80" : "py-4 h-[180px]"}>
+			<div class="py-4">
 				<h3 class={"text-xl font-customHeading font-semibold mb-2"}>{skills.name}</h3>
 
 				{#each skills.skill as { name, value, score }}
