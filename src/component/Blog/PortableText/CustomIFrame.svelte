063821ac15af5d44fd6d63163df087965a146700
harry843 <kelleherharry@gmail.com>
2024-05-29 22:05:37 +0100

    add logo watermark to iframe component

--- src/component/Blog/PortableText/CustomIFrame.svelte
+++ src/component/Blog/PortableText/CustomIFrame.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
+	import Logo from '../../NavBar/Footer/icons/Logo.svelte';
 	export let portableText;
 </script>
 
 <br />
-<div class="pt-4">
-	<slot name="iframe"><iframe class="rounded-md w-full h-96 border border-gray-100 dark:bg-white" frameborder="0" src={portableText?.value.url} title="Embedded visualisation" /></slot>
-</div>
+<div class="pt-4 relative">
+	<span class="absolute top-3 md:top-4 left-0 p-2">
+	  <Logo size={8}/>
+	</span>
+	<iframe 
+	  class="rounded-md w-full h-96 border border-gray-100 dark:bg-white" 
+	  frameborder="0" 
+	  src={portableText?.value.url} 
+	  title="Embedded visualisation" 
+	/>
+  </div>
 {#if portableText?.value.caption}
 	<slot name="caption"><p class="pt-2 pb-10 text-center text-gray-800 text-xs md:text-sm dark:text-slate-100 mx-[5%]">{portableText?.value.caption}</p></slot>
 {/if}
