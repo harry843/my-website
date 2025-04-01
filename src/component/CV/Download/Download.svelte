583302325806b4f7c084d76550e2502da8e3a217
harry843 <kelleherharry@gmail.com>
2025-04-01 14:35:11 +0100

    add CV download + update CV content

---
+++ src/component/CV/Download/Download.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let screenwidth: number;
+
+	const downloadFile = () => {
+		const link = document.createElement('a');
+		link.href = '/Harry Kelleher CV_2025.pdf';
+		link.download = 'Harry_Kelleher_CV.pdf';
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	};
+</script>
+
+<button
+	on:click={downloadFile}
+	class="flex items-center p-2 gap-x-2 text-[0.825rem] font-normal hover:underline hover:text-indigo-700 focus:ring-indigo-100 focus:ring-2 focus:rounded-lg"
+>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-5 h-5 xl:w-6 xl:h-6 stroke-indigo-600"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		stroke-width="2.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+		<polyline points="7 10 12 15 17 10" />
+		<line x1="12" y1="15" x2="12" y2="3" />
+	</svg>
+	{screenwidth >= 430 ? "Download CV" : ""}
+</button>
--- src/component/CV/Header/Header.svelte
+++ src/component/CV/Header/Header.svelte
@@ -1,20 +1,28 @@
-<script>
+<script lang="ts">
+	import Download from '../Download/Download.svelte';
 	import Section from '../Shared/Section/Section.svelte';
 	import Contact from './Contact/Contact.svelte';
+
+	export let screenwidth;
 </script>
 
 <Section>
 	<div class="xxs:flex flex-col text-left lg:flex-row items-left justify-between">
 		<div class="flex flex-col gap-y-2">
-			<h1 class="max-w-max text-3xl font-medium font-customHeading dark:text-black pb-2">Harry Kelleher</h1>
+			<div class="flex gap-x-3.5 xs:gap-x-6">
+				<h1 class="max-w-max text-3xl font-medium font-customHeading dark:text-black pb-2">
+					Harry Kelleher
+				</h1>
+				<Download {screenwidth} />
+			</div>
 			<div class="flex flex-col sm:items-center sm:flex-row gap-x-4 pt-3">
 				<h2 class="text-lg font-medium text-gray-800">
-					Business Intelligence Developer <span class="text-indigo-500">&#8226;</span> Analytics Manager
+					Data Developer <span class="text-indigo-500">&#8226;</span> Analytics Instructor
 				</h2>
 				<span class="text-indigo-500 hidden sm:block text-xl">|</span>
-				<div class="flex flex-col gap-y-2 text-left group relative ">
+				<div class="flex flex-col gap-y-2 text-left group relative">
 					<p class="pt-2 sm:pt-1 dark:text-gray-950">
-						<button>Security Cleared</button><img
+						<button class="text-[0.825rem]">Security Cleared</button><img
 							src={`secure.svg`}
 							alt="security cleared"
 							style="float:left;width:24px;height:24px;margin-right:6px"
