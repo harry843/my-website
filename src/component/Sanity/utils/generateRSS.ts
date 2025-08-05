d9e2b855046780a894b7664a4336cf3f9d5ea5f6
harry843 <kelleherharry@gmail.com>
2025-08-05 16:17:27 +0200

    add RSS feed endpoint

---
+++ src/component/Sanity/utils/generateRSS.ts
@@ -0,0 +1,45 @@
+import genImageUrl from './genImageUrl';
+
+export default function generateRSS(posts, dataset) {
+	const baseUrl =
+		dataset === 'development' ? 'https://staging.harrykelleher.com' : 'https://harrykelleher.com';
+
+	const items = posts
+		.map(
+			(post) => `
+    <item>
+      <title>${escapeXML(post.title)}</title>
+      <link>${baseUrl}/blog/${post.slug}</link>
+      <guid>${baseUrl}/blog/${post.slug}</guid>
+      <pubDate>${new Date(post.publishedAt).toUTCString()}</pubDate>
+      <description><![CDATA[${post.feature}]]></description>
+      ${
+				post.imageUrl
+					? `<enclosure url="${genImageUrl(
+							post.imageUrl,
+							dataset,
+							'?fit=max'
+					  )}" type="image/jpeg" length="0" />`
+					: ''
+			}
+    </item>
+  `
+		)
+		.join('');
+
+	return `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+      <title>Harry Kelleher's Blog</title>
+      <link>${baseUrl}/blog</link>
+      <atom:link href="${baseUrl}/rss.xml" rel="self" type="application/rss+xml" />s
+      <description>Latest Posts from Harry Kelleher</description>
+      <language>en-us</language>
+      ${items}
+    </channel>
+  </rss>`;
+}
+
+function escapeXML(str: string) {
+	return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+}
---
+++ src/routes/rss.xml/+server.ts
@@ -0,0 +1,34 @@
+import { SANITY_PROJECT_ID, SANITY_API_VERSION } from '$env/static/private';
+// import { createClient } from '@sanity/client';
+import { env } from '$env/dynamic/private';
+import generateRSS from '../../component/Sanity/utils/generateRSS';
+
+export const GET = async ({ url }) => {
+  const isLocalOrStaging =
+    url.href.includes('localhost') || url.href.includes('staging.harrykelleher.com');
+
+  const dataset =
+    env.NODE_ENV === 'development' || isLocalOrStaging
+      ? 'development'
+      : 'production';
+
+  const query = encodeURIComponent(`
+    *[_type == 'post']
+	  | order(_createdAt desc) {
+		  title, "slug":slug.current, "publishedAt": _updatedAt, "imageUrl":mainImage.image.asset._ref, "imageCaption":mainImage.caption, "imageAlt":mainImage.alt, feature, tags
+	  }
+  `);
+
+  //const sanityUrl = `https://g2pdrwyj.api.sanity.io/v2022-03-07/data/query/${dataset}?query=${query}`;
+  const sanityUrl = `https://${SANITY_PROJECT_ID}.api.sanity.io/${SANITY_API_VERSION}/data/query/${dataset}?query=${query}`;
+  const res = await fetch(sanityUrl);
+  const { result: posts } = await res.json();
+
+  const xml = generateRSS(posts, dataset);
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/rss+xml'
+    }
+  });
+};
